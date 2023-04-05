535134f875daf753821abd69c45b429987a67db7
Erica Rangel <80468533+ericarcrespo@users.noreply.github.com>
2023-04-04 21:16:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//alert("ok");
+
+var btnMenu = document.querySelector('.btn-menu');
+var menu = document.querySelector('.menu');
+var header = document.querySelector('.header');
+
+//console.log(btnMenu);
+//console.log(menu);
+
+//Evento de clique
+btnMenu.addEventListener('click', function () {
+    //alert('clicou');
+    menu.classList.toggle('menu-open');
+    btnMenu.classList.toggle('x');
+})
+
+//Evento de SCROLL 
+window.addEventListener('scroll', function () {
+    //console.log("rolou");
+    //console.log(window.scrollY);
+
+    if (window.scrollY > 100) {
+        header.classList.add('header-scroll');
+    }
+    else {
+        header.classList.remove('header-scroll');
+    }
+
+})
